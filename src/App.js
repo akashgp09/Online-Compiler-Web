3e887c0aa589ebb19a5f91978f9c3c3720de3339
Akash Gupta <akashgp9@gmail.com>
2020-09-18 06:40:47 +0530

    Added Index and App File

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import Compiler from "./Components/Compiler/Compiler";
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <Compiler />
+      </>
+    );
+  }
 }
-
-export default App;
